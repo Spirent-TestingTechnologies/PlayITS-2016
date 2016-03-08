40c3131fef6e35894e135af3ab3bd77377195702
Artur Keil <arturkeil@zedat.fu-berlin.de>
2016-03-08 12:38:52 +0100

    Removed tag check and added simulating support
    The rfid module now always sends the rfid tag when in listening mode
    even if it does not match the rfid tag set during setup.
    Furthermore it is now possible to use the module without rfid sensor,
    when put into listening mode the module will randomly find a card close
    and send its random generate uid.

--- TTplugin-PhyIO/com.testingtech.ttworkbench.phyio.arduino/PhyIOCarSim/rfid/rfid.ino
+++ TTplugin-PhyIO/com.testingtech.ttworkbench.phyio.arduino/PhyIOCarSim/rfid/rfid.ino
@@ -42,6 +42,11 @@ unsigned int RFID_ENABLED = 0; // Tells if rfid is enabled for scanning
 #define RFID_RST_PIN 9
 MFRC522 mfrc522(RFID_SS_PIN, RFID_RST_PIN); // creates and holds mfrc522 data
 
+#define RFID_PRESENT 1
+#ifndef RFID_PRESENT
+  long randNumber;
+#endif
+
 void setup() {
   XSERIAL.begin(9600, SERIAL_8N1); // Open serial monitor at 9600 baud
 
@@ -53,6 +58,11 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
   establishContact();  // send a byte to establish contact until receiver responds
+
+#ifndef RFID_PRESENT
+  DEBUG_PRINTLN("Simulating RFID");
+  randomSeed(RFID_SS_PIN);
+#endif
 
   DEBUG_PRINTLN("#Entered loop");
 }
@@ -146,33 +156,52 @@ void RFIDFunctionSetup(){
 
 void RFIDFunctionProcess(int id){
     DEBUG_PRINTLN("In RFIDFunctionProcess...");
+    int rfid_tag_tmp[4];
+
+#ifdef RFID_PRESENT    
     if(!mfrc522.PICC_ReadCardSerial()){
       return; // nothing to read
     }
-    unsigned long time = millis();
-    
-    int rfid_tag_tmp[4];
+
     for (byte i = 0; i < mfrc522.uid.size; i++){
       rfid_tag_tmp[i] = (int) mfrc522.uid.uidByte[i]; 
     }
-    
-    if(rfid_tag[0] != 0 && rfid_tag[1] != 0 && rfid_tag[2] != 0 && rfid_tag[3] != 0){
-      for(int i = 0; i < 4; i++){
-        if(rfid_tag[i] != rfid_tag_tmp[i]){
-          DEBUG_PRINT("Tags stimmen nicht überein: ");
-          DEBUG_PRINT(rfid_tag[i]); DEBUG_PRINT(" != ");
-          DEBUG_PRINTLN(rfid_tag_tmp[i]);
-          return;
-        }
-      }
+#else
+    randNumber = random(100);
+    if(randNumber >= 5){
+      return; // nothing to read
     }
 
+    // Generate random rfid uid
+    for (byte i = 0; i < 4; i++){
+      rfid_tag_tmp[i] = random(256); 
+    }
+#endif
+
+// Only send matching tag - currently not used, always sending tag if found
+//    if(rfid_tag[0] != 0 && rfid_tag[1] != 0 && rfid_tag[2] != 0 && rfid_tag[3] != 0){
+//      for(int i = 0; i < 4; i++){
+//        if(rfid_tag[i] != rfid_tag_tmp[i]){
+//          DEBUG_PRINT("Tags stimmen nicht überein: ");
+//          DEBUG_PRINT(rfid_tag[i]); DEBUG_PRINT(" != ");
+//          DEBUG_PRINTLN(rfid_tag_tmp[i]);
+//          return;
+//        }
+//      }
+//    }
+    
+    unsigned long time = millis();
+    
     // ID, RF01, R1, <tag1:int>, <tag2:int>, <tag3:int>, <tag4:int> <timestamp:long>
     XSERIAL.print(0); XSERIAL.print(", ");
     XSERIAL.print(RF01); XSERIAL.print(", ");
     XSERIAL.print(R1); XSERIAL.print(", ");
     for(int i = 0; i < 4; i++){
+#ifdef RFID_PRESENT
       XSERIAL.print(mfrc522.uid.uidByte[i], rfid_format);
+#else
+      XSERIAL.print(rfid_tag_tmp[i], rfid_format);
+#endif
       XSERIAL.print(", ");
     }
     
