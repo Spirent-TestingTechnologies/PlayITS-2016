5ad6fe1b031ceb24589aba51dbe3c8914fbeb863
crimson2005 <crimson2005@outlook.com>
2016-03-23 13:51:37 +0100

    inital commit single TTmanServer ServiceProvider

---
+++ TTplugin-PhyIO/com.testingtech.ttworkbench.phyio.ttmanServerServiceProvider/src/com/testingtech/ttworkbench/phyio/ttmanServerServiceProvider/Testcase.java
@@ -0,0 +1,36 @@
+package com.testingtech.ttworkbench.phyio.ttmanServerServiceProvider;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Testcase {
+	
+	private String testcaseID;
+	private Map<String, List<String>> annotations;
+	private LinkedHashMap<String, String>  parameters = new LinkedHashMap<String, String> ();
+	
+	
+	public Testcase(String testcaseID, LinkedHashMap<String, String> parameters, Map<String, List<String>> annotations){
+		this.testcaseID=testcaseID;
+		this.parameters=parameters;
+		this.annotations=annotations;
+
+	}
+	
+	
+	public String getTestcaseID(){
+		return testcaseID;
+	}
+	
+	public List<String> getAnnotationValues(String annotKey){
+		List<String> values =annotations.get(annotKey);
+		if(values==null){
+			return new ArrayList<String>();
+		}else{
+			return values;
+		}
+	}
+
+}
