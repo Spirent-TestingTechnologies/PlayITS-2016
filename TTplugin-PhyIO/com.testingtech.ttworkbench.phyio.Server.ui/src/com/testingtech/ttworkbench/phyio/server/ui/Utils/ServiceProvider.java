3e875818a2d72deebf0f63769abc8a3b07722e32
davidd90 <d.damm90@gmx.de>
2016-03-30 22:53:24 +0200

    added main class for running the serviceProvider without gui

--- TTplugin-PhyIO/com.testingtech.ttworkbench.phyio.Server.ui/src/com/testingtech/ttworkbench/phyio/server/ui/Utils/ServiceProvider.java
+++ TTplugin-PhyIO/com.testingtech.ttworkbench.phyio.Server.ui/src/com/testingtech/ttworkbench/phyio/server/ui/Utils/ServiceProvider.java
@@ -1,8 +1,16 @@
 package com.testingtech.ttworkbench.phyio.server.ui.Utils;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,21 +26,34 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-public class ServiceProvider {
+public class ServiceProvider implements Runnable{
 	
 	private String TTCN3_NATURE = "com.testingtech.ttworkbench.core.ttcn3nature";
-	
+	private boolean serverIsRunning ;
 	private Map<String,TestModule> moduleMap ;
+	private String workspacePath;
+	private int port;
 	
 	String SEPERATOR= "<SEP>";
 	
-	public ServiceProvider(){
+	
+	private String PROJECTS_REQ = "getProjectsFromWorkspace";
+	private String MODULES_REQ = "getModulesFromFolder";
+	private String TESTCASES_REQ = "getTestcasesFromModule";
+	private String ANNOT_VALUES_TESTCASE_REQ = "getAnnotationValuesForTestcase";
+	private String ANNOT_VALUES_MODUL_REQ = "getAnnotationValuesForModul";
+	private String WORKSPACE_REQ = "getWorkspacePath";
+	
+	public ServiceProvider(String workspacePath, int port){
+		
+		
 		moduleMap= new HashMap<String,TestModule>();
+		this.workspacePath=workspacePath;
+		this.port=port;
 	}
 	
-	//send the existing project names for the given workspacePath
-	public void sendProjectNames(BufferedWriter bWriter, String workspacePath) {
-		
+	private void sendProjectNames(BufferedWriter bWriter, String workspacePath) {
+
 		//check if the project in the workspace is a TTCN3 Project. Its a TTCN3 project if the project nature is set
 		File workspace = new File(workspacePath);
 		if(workspace.exists() && workspace.isDirectory()) {
@@ -54,22 +75,23 @@ public class ServiceProvider {
 									if(node.getNodeType() == Node.ELEMENT_NODE) {
 										Element element = (Element) node;
 										
-										String strNature = element.getElementsByTagName("nature").item(0).getTextContent();
-										if(strNature.equals(TTCN3_NATURE)) {
-											bWriter.write(project.getName() + SEPERATOR);
+										Node elemNode = element.getElementsByTagName("nature").item(0);
+										if(elemNode!=null){
+											String strNature=elemNode.getTextContent();
+											if(strNature.equals(TTCN3_NATURE)) {
+												bWriter.write(project.getName() + SEPERATOR);
+											}
 										}
+										
 										
 									}
 								}
 								
 							} catch (ParserConfigurationException e) {
-								// TODO Auto-generated catch block
 								e.printStackTrace();
 							} catch (SAXException e) {
-								// TODO Auto-generated catch block
 								e.printStackTrace();
 							} catch (IOException e) {
-								// TODO Auto-generated catch block
 								e.printStackTrace();
 							}
 							
@@ -84,7 +106,6 @@ public class ServiceProvider {
 			bWriter.newLine();
 			bWriter.flush();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -92,8 +113,8 @@ public class ServiceProvider {
 		
 	}
 	
-	//send the moduleNames for the given path to a project
-	public void sendModuleNames(BufferedWriter bWriter,String path){
+	private void sendModuleNames(BufferedWriter bWriter,String path){
+
 		List<TestModule> testModules = new ArrayList<TestModule>();
   		File folder = new File(path);
   		
@@ -139,8 +160,7 @@ public class ServiceProvider {
 	}
 
 
-	//sends the testcases for the given module name
-	public void sendTestcases(BufferedWriter bWriter, String modName) {
+	private void sendTestcases(BufferedWriter bWriter, String modName) {
 
 		try{
 			if(modName==null||modName.isEmpty()){
@@ -160,8 +180,8 @@ public class ServiceProvider {
 		}			
 	}
 	
-	//send the annotation for given testcase data
-	public void sendAnnotationValuesForTestcase(BufferedWriter bWriter,String data) {
+	private void sendAnnotationValuesForTestcase(BufferedWriter bWriter,String data) {
+
 		String[] reqValues = data.split(SEPERATOR);
 		try{
 			if(reqValues.length==3){
@@ -187,8 +207,8 @@ public class ServiceProvider {
 		}			
 	}
 	
-	//send the annotation for given modul data
-	public void sendAnnotationValuesForModul(BufferedWriter bWriter,String data) {
+	private void sendAnnotationValuesForModul(BufferedWriter bWriter,String data) {
+
 		String[] reqValues = data.split(SEPERATOR);
 		try{
 			if(reqValues.length==2){
@@ -212,8 +232,8 @@ public class ServiceProvider {
 		}			
 	}
 	
-	//send the workspacePath to the client
-	public void sendWorkspacePath(BufferedWriter bWriter, String workspacePath) {
+	private void sendWorkspacePath(BufferedWriter bWriter, String workspacePath) {
+
 		try {
 			bWriter.write(workspacePath);
 			bWriter.write("\n");
@@ -223,6 +243,77 @@ public class ServiceProvider {
 		}
 		
 	}
+
+	@Override
+	public void run() {
+		serverIsRunning=true;
+	 
+	    	
+    	while(serverIsRunning){   
+    		try{
+    			ServerSocket servsock = new ServerSocket(port);
+
+    			Socket sock = servsock.accept();
+    		    InputStream is = sock.getInputStream();
+    		    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+    			OutputStream os = sock.getOutputStream();
+    			  
+    			BufferedWriter bWriter = new BufferedWriter(new OutputStreamWriter(os));
+    		    String request ;
+    		    while((request = br.readLine())!=null){
+    		    	if(request.equals(MODULES_REQ)){
+    		    	  	String relPath = br.readLine();				// client sends relative path
+    		    	  	if(relPath!=null){
+    		    	  		sendModuleNames(bWriter,workspacePath+"\\"+relPath);
+    		    	  	}
+    		    	} else if(request.endsWith(TESTCASES_REQ)){
+    		    		String modName = br.readLine();				// client sends modulename
+    		    	  	if(modName!=null){
+    		    	  		sendTestcases(bWriter,modName);
+    		    	  	}
+    		    	} else if(request.endsWith(ANNOT_VALUES_TESTCASE_REQ)){
+		    	  		String modName = br.readLine();				// client sends modulename testcasename and annotation
+		    	  		if(modName!=null) {
+		    	  			sendAnnotationValuesForTestcase(bWriter,modName);
+		    	  		}
+    		    	} else if(request.endsWith(ANNOT_VALUES_MODUL_REQ)) {
+    		    		String modulAndAnnotation = br.readLine();	
+    		    		if(modulAndAnnotation!=null) {
+    		    			sendAnnotationValuesForModul(bWriter, modulAndAnnotation);
+    		    		}
+    		    	} else if(request.endsWith(WORKSPACE_REQ)) {
+    		    	  	sendWorkspacePath(bWriter, workspacePath);
+    		    	} else if(request.endsWith(PROJECTS_REQ)) {
+    		    	  	sendProjectNames(bWriter, workspacePath);
+    		    	 }
+    		    	  	
+ 
+    		      }
+    		      sock.close();
+    		      servsock.close();
+    		      
+	    		
+	    	}catch(SocketException se){	    		
+	    	}catch(Exception ex) {
+				ex.printStackTrace();
+				return;
+			}
+    	}
+	    	    		  
+    				    
+		
+	}
+
+	public void stop(){
+		serverIsRunning= false;
+	}
+
+	public boolean isRunning() {
+		return serverIsRunning;
+	}
+	
+	
+
 	
 	
 }
--- TTplugin-PhyIO/com.testingtech.ttworkbench.phyio.Server.ui/src/com/testingtech/ttworkbench/phyio/server/ui/parts/ServerMainPart.java
+++ TTplugin-PhyIO/com.testingtech.ttworkbench.phyio.Server.ui/src/com/testingtech/ttworkbench/phyio/server/ui/parts/ServerMainPart.java
@@ -1,15 +1,7 @@
 package com.testingtech.ttworkbench.phyio.server.ui.parts;
  
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -33,19 +25,17 @@ public class ServerMainPart {
 	private Text textWorkspacePath;
 	private Label lblConnectionStatus;
 	private int PORT=22782;
-	
-	private String PROJECTS_REQ = "getProjectsFromWorkspace";
-	private String MODULES_REQ = "getModulesFromFolder";
-	private String TESTCASES_REQ = "getTestcasesFromModule";
-	private String ANNOT_VALUES_TESTCASE_REQ = "getAnnotationValuesForTestcase";
-	private String ANNOT_VALUES_MODUL_REQ = "getAnnotationValuesForModul";
-	private String WORKSPACE_REQ = "getWorkspacePath";
+
 
 	private Process ttmanProcess;
 
+	private ServiceProvider serviceProvider;
+	
+	IExecutionServer client;
+	
+
 	@Inject
 	public ServerMainPart() {
-		//TODO Your code here
 	}
 	
 	@PostConstruct
@@ -58,13 +48,13 @@ public class ServerMainPart {
 		new Label(parent, SWT.NONE);
 		
 		Label lblServerPath = new Label(parent, SWT.NONE);
-		lblServerPath.setText("Pfad:");
+		lblServerPath.setText("TTmanServer-Pfad:");
 		
 		textServerPath = new Text(parent, SWT.BORDER);
 		textServerPath.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
 		
 		Label lblWorkspacePath = new Label(parent, SWT.NONE);
-		lblWorkspacePath.setText("Workspace:");
+		lblWorkspacePath.setText("Workspace-Pfad:");
 		
 		textWorkspacePath = new Text(parent, SWT.BORDER);
 		textWorkspacePath.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
@@ -76,11 +66,11 @@ public class ServerMainPart {
 		btnStart.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				
+
 		    	String serverPath = textServerPath.getText();
 		    	String workspacePath = textWorkspacePath.getText();
-						    	
 		    	System.out.println("start server ...");
+				
 							
 		    	try {
 		    		//starts the server if your operating system is windows, change parameters when using linux
@@ -95,74 +85,32 @@ public class ServerMainPart {
 		    	} else{
 		    		System.out.println("Process started");
 		    	}
-						    	
+						 
+		    	serviceProvider = new ServiceProvider(workspacePath,PORT);
 		    	//thread for server socket, necessary for the service provider
-		    	Thread thread = new Thread(new Runnable() {
-					@Override
-					public void run() {
-							
-						try {
-			    
-					    	while(true){
-				    			ServiceProvider provider = new ServiceProvider();
-				    			ServerSocket servsock = new ServerSocket(PORT);
-					    		      
-				    			Socket sock = servsock.accept();
-				    		    InputStream is = sock.getInputStream();
-				    		    BufferedReader br = new BufferedReader(new InputStreamReader(is));
-				    			OutputStream os = sock.getOutputStream();
-					    			  
-				    			BufferedWriter bWriter = new BufferedWriter(new OutputStreamWriter(os));
-				    		    String request ;
-				    		    while((request = br.readLine())!=null){
-				    		    	if(request.equals(MODULES_REQ)){
-				    		    	  	String relPath = br.readLine();				// client sends relative path
-				    		    	  	if(relPath!=null){
-				    		    	  		provider.sendModuleNames(bWriter,workspacePath+"\\"+relPath);
-				    		    	  	}
-				    		    	} else if(request.endsWith(TESTCASES_REQ)){
-				    		    		String modName = br.readLine();				// client sends modulename
-				    		    	  	if(modName!=null){
-				    		    	  		provider.sendTestcases(bWriter,modName);
-				    		    	  	}
-				    		    	} else if(request.endsWith(ANNOT_VALUES_TESTCASE_REQ)){
-			    		    	  		String modName = br.readLine();				// client sends modulename testcasename and annotation
-			    		    	  		if(modName!=null) {
-			    		    	  			provider.sendAnnotationValuesForTestcase(bWriter,modName);
-			    		    	  		}
-				    		    	} else if(request.endsWith(ANNOT_VALUES_MODUL_REQ)) {
-				    		    		String modulAndAnnotation = br.readLine();	 //client sends modulename and annotation
-				    		    		if(modulAndAnnotation!=null) {
-				    		    			provider.sendAnnotationValuesForModul(bWriter, modulAndAnnotation);
-				    		    		}
-				    		    	} else if(request.endsWith(WORKSPACE_REQ)) {
-				    		    	  	provider.sendWorkspacePath(bWriter, workspacePath);
-				    		    	} else if(request.endsWith(PROJECTS_REQ)) {
-				    		    	  	provider.sendProjectNames(bWriter, workspacePath);
-				    		    	}
-					    		}
-				    		    sock.close();
-				    		    servsock.close();
-						    }
-						} catch(Exception ex) {
-							ex.printStackTrace();
-						} 
-					}
-				});
-					
-				thread.start();
-			    	
-			    
+		    	Thread thread = new Thread(serviceProvider);
+		    	thread.start();
 			}
 		});
 		
-		btnStart.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
-		btnStart.setText("Start");
-		new Label(parent, SWT.NONE);
-		new Label(parent, SWT.NONE);
+			btnStart.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+			btnStart.setText("Start");
+			new Label(parent, SWT.NONE);
+		
+		Button btnStop = new Button(parent, SWT.NONE);
+		btnStop.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if(serviceProvider.isRunning()){
+					serviceProvider.stop();
+				}
+			}
+		});
+		btnStop.setText("Stop");
 		
 		lblConnectionStatus = new Label(parent, SWT.NONE);
 		lblConnectionStatus.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 3, 1));
+
 	}
 	
 }
